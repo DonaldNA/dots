598586b73fed6b71dd72220d24569e17677bc63a
Donald <donaldnallen87@gmail.com>
2021-12-27 14:56:17 -0800

    Install Scripts

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+declare -a dots
+
+dots=(".vimrc" ".bashrc" ".tmux.conf")
+
+
+rmdir "$HOME"/dots/backup
+mkdir "$HOME"/dots/backup
+
+for i in "${dots[@]}"
+do
+  echo "Working on $i"
+  prod_dot="$HOME"/"$i"
+  cp "$prod_dot" "$HOME"/dots/backup/"$i".bak
+  rm "$prod_dot"
+  ln -s "$HOME"/dots/"$i" "$HOME"
+done
+
