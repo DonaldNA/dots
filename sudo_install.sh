5c0856976ebec08e3b3872688060fe35ba10ed99
Donald <donaldnallen87@gmail.com>
2022-01-01 16:54:24 +0000

    fix zsh

--- sudo_install.sh
+++ sudo_install.sh
@@ -1,12 +1,12 @@
 echo "Checking if z.sh exists..."
 
-if [ ! -d /usr/bin/local/ ]
+if [ ! -d /usr/local/bin ]
 then
     echo "Creating backup dir"
-    mkdir /usr/bin/local
+    mkdir /usr/local/bin
 fi
 
-if [ ! -f /usr/bin/local/z.sh ]
+if [ ! -f /usr/local/bin/z.sh ]
 then
     echo "Getting z.sh"
     wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O /usr/bin/local/z.sh
